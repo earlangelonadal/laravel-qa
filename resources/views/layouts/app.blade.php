6a9273957cf3766900117b8efe1419a6cc687847
unknown <earlangelonadal@gmail.com>
2021-02-11 20:06:58 -0800

    add author info in question title in question item

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,4 +78,18 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
+
+<!-- Footer -->
+<footer class="bg-light text-center text-lg-start">
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2020 Copyright:
+      <a class="text-dark">EA Tech</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+  <!-- Footer -->
+
+
 </html>
